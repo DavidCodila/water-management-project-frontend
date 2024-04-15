dd8121876b622b77dea82d77f3e65956fb938693
David <david@Davids-MacBook-Pro.local>
2024-04-15 16:00:58 +1000

    Added specific user print bill functionality

--- src/PrintBill.tsx
+++ src/PrintBill.tsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, Container } from '@mui/material';
 import Typography from '@mui/material/Typography';
+import { useLocation } from 'react-router-dom';
+import { id } from './id.tsx';
 
 export default function PrintBill() {
-    const userID = "2131241";
-    const [waterUsed, setWaterUser] = useState("");
-    const [cost, setCost] = useState("");
-    const userIdJSON = {"userID": userID}
+  const [waterUsed, setWaterUser] = useState("");
+  const [cost, setCost] = useState("");
+
+  useEffect(() => {
+    const userID = id[id.length - 1];
     var URL = "http://localhost:81/water-accounts/" + userID + "/bill";
     fetch(URL, {
       method: 'GET',
@@ -24,26 +27,30 @@ export default function PrintBill() {
         setWaterUser("Error");
         setCost("Error")
       });
-
-  return <Container maxWidth="sm">
-    <Typography variant="h3" gutterBottom sx={{ display: 'block', textAlign: 'center' }}>
-      Bill
-    </Typography>
-    <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
-      <Typography variant="h5" gutterBottom sx={{ display: 'inline', textAlign: 'center' }}>
-        Water (L)
-      </Typography>
-      <Typography variant="h5" gutterBottom sx={{ display: 'block', textAlign: 'center' }}>
-        Price ($)
-      </Typography>
-    </Box>
-    <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
-      <Typography variant="h5" gutterBottom sx={{ display: 'inline', textAlign: 'center' }}>
-        {waterUsed}L
-      </Typography>
-      <Typography variant="h5" gutterBottom sx={{ display: 'block', textAlign: 'center' }}>
-        ${cost}
-      </Typography>
-    </Box>
-  </Container>;
+      return () =>{
+      }
+  })
+  return (
+    <Container maxWidth="sm">
+        <Typography variant="h3" gutterBottom sx={{ display: 'block', textAlign: 'center' }}>
+          Bill
+        </Typography>
+        <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
+          <Typography variant="h5" gutterBottom sx={{ display: 'inline', textAlign: 'center' }}>
+            Water (L)
+          </Typography>
+          <Typography variant="h5" gutterBottom sx={{ display: 'block', textAlign: 'center' }}>
+            Price ($)
+          </Typography>
+        </Box>
+        <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
+          <Typography variant="h5" gutterBottom sx={{ display: 'inline', textAlign: 'center' }}>
+            {waterUsed}L
+          </Typography>
+          <Typography variant="h5" gutterBottom sx={{ display: 'block', textAlign: 'center' }}>
+            ${cost}
+          </Typography>
+        </Box>
+     </Container>
+  )
 }
