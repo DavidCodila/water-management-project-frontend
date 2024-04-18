07b775c1b542d259aafe01de9082283e125088c5
david <Davids-MacBook-Pro.local>
2024-04-18 12:17:57 +1000

    Added can not find id error to add people page

--- src/AddPeopleToAccountPage.tsx
+++ src/AddPeopleToAccountPage.tsx
@@ -1,41 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Container, TextField } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { id } from "./id.tsx";
 
 export default function AddPeopleToAccountPage() {
   const [peopleAdded, setPeopleAdded] = useState("");
-
-  function handleAppPeople() {
-    const userID = id[id.length - 1];
-    const peopleToAddJSON = {"peopleToAdd": peopleAdded}
-    var URL = "http://localhost:81/water-accounts/" + userID;
-    fetch(URL, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(peopleToAddJSON),
-    })
-      .then(response => response.json())
-      .then(data => {
-        //replace regex expression strips off (") form the start and end of the message
-        alert(JSON.stringify(data.response).replace(/^"(.+(?="$))"$/, '$1'))
+    function handleAppPeople() {
+      const userID = id[id.length - 1]+1;
+      const peopleToAddJSON = {"peopleToAdd": peopleAdded};
+      //var error;
+      //const errorJson = {"error":error}
+      var URL = "http://localhost:81/water-accounts/" + userID;
+      fetch(URL, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(peopleToAddJSON),
       })
-      .catch(error => {
-        console.error(error);
-      });
-  }
+        .then(response => response.json())
+        .then(data => {
+          //replace regex expression strips off (") form the start and end of the message
+          if (data.response !== undefined) {
+            alert(JSON.stringify(data.response).replace(/^"(.+(?="$))"$/, '$1'))
+          }
+          alert(JSON.stringify(data.error))
+        })
+        //does not enter into this blok of error occurs?
+        .catch(error => {
+          console.log(error.message);
+        });
+    }
 
-  function ButtonLink() {
-    return <Link to={"/billPage/"}><Button variant="contained">Print bill</Button></Link>;
-  }
-  
-  return (
-    <Container maxWidth="sm">
-      <TextField id="peopleToAddTextField" label="People to add" sx={{ display: "flex", textAlign: 'center' }} onChange={(e) => setPeopleAdded(e.target.value)} />
-      <Button variant="contained" onClick= {handleAppPeople}>App people</Button>
-      <ButtonLink />
-    </Container>
-  );
+    function ButtonLink() {
+      return <Link to={"/billPage/"}><Button variant="contained">Print bill</Button></Link>;
+    }
+    
+    return (
+      <Container maxWidth="sm">
+        <TextField id="peopleToAddTextField" label="People to add" sx={{ display: "flex", textAlign: 'center' }} onChange={(e) => setPeopleAdded(e.target.value)} />
+        <Button variant="contained" onClick= {handleAppPeople}>App people</Button>
+        <ButtonLink />
+      </Container>
+    );
 }
